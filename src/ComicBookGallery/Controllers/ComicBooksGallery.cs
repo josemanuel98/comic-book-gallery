a6dbb9421cb1f19441526f28321171f57c39bc7a
josemanuel98 <josemanuelmartinezlopez@live.com.mx>
2016-10-15 13:59:40 -0500

    Added detail action method

--- src/ComicBookGallery/Controllers/ComicBooksGallery.cs
+++ src/ComicBookGallery/Controllers/ComicBooksGallery.cs
@@ -8,6 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksGallery : Controller
     {
-
+        public string Detail()
+        {
+            return "Hola Manuel";
+        }
     }
 }
