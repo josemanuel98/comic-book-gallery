bb35f3d4165ce6c4bf781655d0e9ce954cc66168
josemanuel98 <josemanuelmartinezlopez@live.com.mx>
2016-10-15 18:29:10 -0500

    Correction of Controller name

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    public class ComicBooksGallery : Controller
+    public class ComicBooksController : Controller
     {
         public string Detail()
         {
